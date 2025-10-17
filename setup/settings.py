1d9e68569a3ee60354c16ae6ceaf0dd3f73c1cd4
MarquitoEmanuel <marcos689028@gmail.com>
2025-10-17 15:16:52 -0330

    app aluno criado

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.aluno.apps.AlunoConfing'
+    'apps.aluno.apps.AlunoConfig'
 ]
 
 MIDDLEWARE = [
