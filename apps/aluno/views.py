dbbe7c0f3608ee11f9b74e043ea358fc238760d8
MarquitoEmanuel <marcos689028@gmail.com>
2026-01-09 15:46:39 -0330

    lista

--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.aluno.forms import AlunoForms
+from apps.aluno.models import Aluno
 
 def index(request):
     return render(request, 'index.html')
 
+def lista(request):
+    alunos = Aluno.objects.all()
+    return render(request, "aluno/lista.html", {"alunos": alunos})
+
 def cadastro(request):
-    return render(request, 'aluno/cadastro.html')
+    form = AlunoForms()
+
+    if request.method == 'POST':
+        form = AlunoForms(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("aluno/lista")
+    
+    return render(request, "aluno/cadastro.html", {"form": form})
+
+def edita(request, id):
+    aluno = Aluno.objects.get(id=id)
+    form = AlunoForms(instance=aluno)
+
+    if request.method == 'POST':
+        form = AlunoForms(request.POST, instance=aluno)
+
+        if form.is_valid():
+            form.save()
+            return redirect("aluno/lista")
+    
+    return render(request, "aluno/edita_html", {"form":form, "aluno_id":id})
